07d1d4f72506755d02f81978f9a27f5387a159be
Juraj Kirchheim <back2dos@googlemail.com>
2019-09-19 16:41:58 +0200

    Add HSV support.

--- src/tink/color/Color.hx
+++ src/tink/color/Color.hx
@@ -4,10 +4,62 @@ import tink.color.Channel;
 
 abstract Color(Int) {
 
+	public var hue(get, never):Float;
+	  function get_hue():Float
+			return compute(function (r, g, b, min, max) return switch max - min {
+				case 0: Math.NaN;
+				case delta:
+					inline function calc(c1, c2, offset = .0)
+						return (360 + 60 * (offset + (c1 - c2) / delta)) % 360;
+
+					if (r == max) calc(g, b);
+					else if (g == max) calc(b, r, 2);
+					else calc(r, g, 4);
+			});
+
+	@:extern inline function compute(f):Float {
+		var r = red,
+				g = green,
+				b = blue;
+
+		var min = Math.min(r, Math.min(g, b)),
+				max = Math.max(r, Math.max(g, b));		
+
+		return f(r, g, b, min, max);
+	}
+
+	public var saturation(get, never):Float;
+	  function get_saturation() return compute(
+			function (r, g, b, min, max) return switch max - min {
+				case 0: 0;
+				case v: v / max;
+			}
+		);
+
+	public var value(get, never):Float;
+	  function get_value()
+			return compute(function (r, g, b, min, max) return max / 0xFF);
+
+	public var red(get, never):Int;
+		inline function get_red()
+			return get(RED);
+
+	public var green(get, never):Int;
+		inline function get_green()
+			return get(GREEN);
+
+	public var blue(get, never):Int;
+		inline function get_blue()
+			return get(BLUE);
+
+	public var alpha(get, never):Int;
+		inline function get_alpha()
+			return get(ALPHA);
+
 	inline function new(i)
 		this = i;
 
-	@:arrayAccess public function get(chan:Channel):ChannelValue
+	@:arrayAccess public inline function get(chan:Channel):ChannelValue
 		return this >> (chan : Int);
 
 	public function with(chan:Channel, value:ChannelValue):Color 
@@ -21,6 +73,38 @@ abstract Color(Int) {
 
 	static public inline function rgba(r:ChannelValue, g:ChannelValue, b:ChannelValue, a:ChannelValue) 
 		return new Color(a << ALPHA | r << RED | g << GREEN | b << BLUE);
+
+	static public function hsv(h:Float, s:Float, v:Float)
+		return hsva(h, s, v, ChannelValue.FULL);
+
+	static public function hsva(h:Float, s:Float, v:Float, a:ChannelValue) 
+		return 
+			if (s == 0) {
+				var val:ChannelValue = Math.round(v * 0xFF);
+				rgba(val, val, val, a);
+			}
+			else {
+				inline function make(r:Float, g:Float, b:Float)
+					return rgba(Math.round(r * 0xFF), Math.round(g * 0xFF), Math.round(b * 0xFF), a);
+
+				h /= 360;
+
+				var sector = Std.int(h * 6);
+				var f = h * 6 - sector;
+
+				var p = v * (1 - s),
+				    q = v * (1 - f * s),
+				    t = v * (1 - (1 - f) * s);
+
+				switch sector {
+					case 0: make(v, t, p);
+					case 1: make(q, v, p);
+					case 2: make(p, v, t);
+					case 3: make(p, q, v);
+					case 4: make(t, p, v);
+					default: make(v, p, q);
+				}
+			}
 
 	public function mix(that:Color, ?factor = .5) {
 
--- tests/Colors.hx
+++ tests/Colors.hx
@@ -1,8 +1,79 @@
 package ;
 
+import tink.color.*;
+
 @:asserts
 class Colors {
+
+  static final BLACK   = Color.rgb(0x00, 0x00, 0x00);
+  static final RED     = Color.rgb(0xFF, 0x00, 0x00);
+  static final YELLOW  = Color.rgb(0xFF, 0xFF, 0x00);
+  static final GREEN   = Color.rgb(0x00, 0xFF, 0x00);
+  static final CYAN    = Color.rgb(0x00, 0xFF, 0xFF);
+  static final BLUE    = Color.rgb(0x00, 0x00, 0xFF);
+  static final PINK    = Color.rgb(0xFF, 0x00, 0xFF);
+  static final WHITE   = Color.rgb(0xFF, 0xFF, 0xFF);
+
   public function new() {}
+
+  public function testHue() {
+    asserts.assert(Math.isNaN(BLACK.hue));
+    asserts.assert(Math.isNaN(WHITE.hue));    
+    asserts.assert(Math.isNaN(BLACK.mix(WHITE).hue));
+
+    asserts.assert(RED.hue == 0);    
+    asserts.assert(YELLOW.hue == 60);
+    asserts.assert(GREEN.hue == 120);
+    asserts.assert(CYAN.hue == 180);
+    asserts.assert(BLUE.hue == 240);
+    asserts.assert(PINK.hue == 300);
+
+    asserts.assert(BLUE.hue == BLUE.mix(BLACK).hue);
+    
+    return asserts.done();
+  }
+
+  public function testValue() {
+
+    asserts.assert(BLACK.value == 0);
+    asserts.assert(RED.value == 1);    
+    asserts.assert(YELLOW.value == 1);
+    asserts.assert(GREEN.value == 1);
+    asserts.assert(CYAN.value == 1);
+    asserts.assert(BLUE.value == 1);
+    asserts.assert(PINK.value == 1);
+    asserts.assert(WHITE.value == 1);
+
+    return asserts.done();    
+  }
+
+  public function testSaturation() {
+    asserts.assert(BLACK.saturation == 0);
+    asserts.assert(RED.saturation == 1);    
+    asserts.assert(YELLOW.saturation == 1);
+    asserts.assert(GREEN.saturation == 1);
+    asserts.assert(CYAN.saturation == 1);
+    asserts.assert(BLUE.saturation == 1);
+    asserts.assert(PINK.saturation == 1);
+    asserts.assert(WHITE.saturation == 0);
+
+    return asserts.done();    
+  }
+
+  @:include public function testRGBtoHSV() {
+    for (i in 0...10) {
+      
+      var c1 = Color.rgb(Std.random(0x100), Std.random(0x100), Std.random(0x100));
+      var c2 = Color.hsv(c1.hue, c1.saturation, c1.value);
+
+      asserts.assert(c1.red == c2.red);
+      asserts.assert(c1.blue == c2.blue);
+      asserts.assert(c1.green == c2.green);
+      asserts.assert(c1.alpha == c2.alpha);
+    }
+    return asserts.done();    
+  }
+
   public function testAll() {
     //TODO: do something here
     return asserts.done();
